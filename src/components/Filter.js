4b9a22238405fadbff9de666198b7573a3c2eb1b
María Molina García <86959823+mariamolinagarcia@users.noreply.github.com>
2021-10-04 17:00:39 +0200

    responsive

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -22,8 +22,8 @@ const Filter = (props) =>{
                 <input className='form__input' type='text' value={props.inputSearch} placeholder='Busca tu personaje favorito...' onChange={handleInput}/>
             </label>
             <label>
-                Filtra por especie:
-            <select className='form__input' name='species' id='species' value={props.searchSpecies} onChange={handleSelectSpecies}>
+                
+            <select className='form__select' name='species' id='species' value={props.searchSpecies} onChange={handleSelectSpecies}>
                 
             <option value='all'>Todos</option>
             <option value='Alien'>Alien</option>
